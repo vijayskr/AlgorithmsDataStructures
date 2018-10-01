670b51d86715f7c94a6f84c0403441cd55be6229
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2018-10-01 16:36:29 -0500

    Commit Latest

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    const arr = str.split('');
+/*     const arr = str.split('');
     arr.reverse();
-    return arr.join('');
+    return arr.join(''); */
+
+    //return str.split('').reverse().join('');
 }
 
 module.exports = reverse;
